116adaa770365100e9014fbab64b7e663b8640d6
Alex <alexxcamargo1000@gmail.com>
2023-06-14 09:59:15 -0300

    feat: styles portfolio page

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,7 +1,20 @@
 export default function Portfolio() {
   return (
-    <div>
-      <h1>portfolio</h1>
+    <div className="flex flex-1 justify-center">
+      <div className="max-w-5xl w-full mt-16">
+        <h1 className=" pl-1 text-4xl font-montserrat font-bold ">Portfolio</h1>
+
+        <div className="my-8 grid grid-cols-4 gap-4">
+          <div className='w-60 h-60 bg-neutral-900 rounded-lg' />
+          <div className='w-60 h-60 bg-neutral-900 rounded-lg' />
+          <div className='w-60 h-60 bg-neutral-900 rounded-lg' />
+          <div className='w-60 h-60 bg-neutral-900 rounded-lg' />
+          <div className='w-60 h-60 bg-neutral-900 rounded-lg' />
+          <div className='w-60 h-60 bg-neutral-900 rounded-lg' />
+          <div className='w-60 h-60 bg-neutral-900 rounded-lg' />
+          <div className='w-60 h-60 bg-neutral-900 rounded-lg' />
+        </div>
+      </div>
     </div>
   )
 }
